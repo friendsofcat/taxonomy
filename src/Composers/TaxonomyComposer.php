8318904479d6681b2de599ce2199f315be0d15ce
Brandon Ferens <brandon@kirschbaumdevelopment.com>
2021-01-28 13:46:39 -0800

    Updates for Laravel 8 and PSR-4 standards

---
+++ src/Composers/TaxonomyComposer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Trexology\Taxonomy\Composers;
+
+use Config;
+
+class TaxonomyComposer
+{
+    public function compose($view)
+    {
+        $prefix = rtrim(Config::get('taxonomy::route_prefix'), '.') . '.';
+
+        $layout = (object) [
+            'extends' => Config::get('taxonomy::config.layout.extends'),
+            'header' => Config::get('taxonomy::config.layout.header'),
+            'content' => Config::get('taxonomy::config.layout.content'),
+        ];
+
+        $view->with(['prefix' => $prefix, 'layout' => $layout]);
+    }
+}
